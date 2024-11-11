7acb1cdd9c1717b83402364583134613d6d5ec9f
JairGarcia29 <jamesjair29@gmail.com>
2024-11-10 23:50:52 -0500

    Integrantes

--- TrabajoS13/Program.cs
+++ TrabajoS13/Program.cs
@@ -1,4 +1,9 @@
-﻿
+﻿//
+//Chumbiauca Quispe , Maria Fernando
+//Alarcon Pinao , Andre Adolfo
+//Garcia Osorio , James Hgun Jair
+//Casas , Fabian 
+// 
 using TrabajoS13;
 
 Desayuno d = new Desayuno();
