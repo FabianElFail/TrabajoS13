bf99c27105eb5f0c012fd912641c3a366050f08a
ANDRE04PROGRAMER <andre.alarcon.2004@gmail.com>
2024-11-12 23:35:55 -0500

    Funcion eliminar parche

--- TrabajoS13/Program.cs
+++ TrabajoS13/Program.cs
@@ -1,24 +1,17 @@
-﻿using TrabajoS13;
-
+﻿using System.Drawing;
+using TrabajoS13;
 Desayuno d = new Desayuno();
 d.Nombre = "Continental";
 d.Precio = 20.5;
 d.Dias = "lun,mar,mie";
-
 Console.WriteLine(d);
-
 String[] lista = d.ListaDias();
 foreach (string str in lista)
 {
     Console.WriteLine(str);
 }
-
-
-
-
 int menu_number, desayuno_length = 0, desayuno2_length = 0, elim=0;
 string[] desayuno = new string[desayuno_length];//creacion de array
-
 do //Bucle del menú
 {
     do
@@ -66,27 +59,38 @@ do //Bucle del menú
             Console.ReadKey();
             break;
         case 4:
-            Console.Clear();
-            Console.WriteLine("¿Qué elemento desea eliminar?");
-            Console.Write("Inserte el nombre del desayuno: ");
-            string nombre_desayuno = Console.ReadLine();
-            elim = Array.IndexOf(desayuno, nombre_desayuno);
-            string[] desayuno2 = new string[desayuno_length];//creacion de array 
-            desayuno2 = desayuno; desayuno2_length = desayuno_length - 1;
-            Array.Resize(ref desayuno, desayuno2_length);       
-            for (int i = 0, i2=0; i2 < desayuno2.Length; i2++)
-            {
-                if (i2 != elim)
+                Console.Clear();
+                Console.WriteLine("¿Qué elemento desea eliminar?");
+                Console.Write("Inserte el nombre del desayuno: ");
+                string nombre_desayuno = Console.ReadLine();
+                if (Array.Exists(desayuno, element => element == nombre_desayuno))
                 {
-                    desayuno[i] = desayuno2[i2];
-                    i++;
+                    elim = Array.IndexOf(desayuno, nombre_desayuno);
+                    string[] desayuno2 = new string[desayuno_length];//creacion de array 
+                    desayuno2 = desayuno; 
+                    desayuno2_length = desayuno_length - 1;
+                    Array.Resize(ref desayuno, desayuno2_length);
+                    for (int i = 0, i2 = 0; i2 < desayuno2.Length; i2++)
+                    {
+                        if (i2 != elim)
+                        {
+                            desayuno[i] = desayuno2[i2];
+                            i++;
+                        }
+                        else { }
+                    }
+                    desayuno_length = desayuno2_length;
+                    Console.WriteLine($"Se eliminó la posición {elim}, es decir el {elim + 1}° elemento");
+                    Console.WriteLine("Presione cualquier tecla para continuar");
+                    Console.ReadKey();
                 }
                 else
-                { }
-            }
-            Console.WriteLine($"Se eliminó la posición {elim}, es decir el {elim+1}° elemento");          
-            Console.WriteLine("Presione cualquier tecla para continuar");
-            Console.ReadKey();
+                {
+                    Console.WriteLine("Elemento no encontrado");
+                    Console.WriteLine("");
+                    Console.WriteLine("Presione cualquier tecla para continuar");
+                    Console.ReadKey();
+                }
             break;
         case 5: //Salir del programa
             Console.Clear();
